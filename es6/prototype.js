19be9fa7b560042be0f4754eacf8bf0b7f184982
Daniel D. Zhang <dzhang.idf@gmail.com>
2016-07-21 23:41:11 -0700

    Create prototype.js

---
+++ es6/prototype.js
@@ -0,0 +1,7 @@
+/*
+Wiki: Prototype-based programming is a style of object-oriented programming in which behaviour reuse (known as 
+inheritance)  is performed via a process of reusing existing objects via delegation that serve as prototypes.
+This model can also  be known as prototypal, prototype-oriented, classless, or instance-based programming. 
+Delegation is the language feature that supports prototype-based programming.
+*/
+
